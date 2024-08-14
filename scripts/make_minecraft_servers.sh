cc925d74146dafd1754d2378b5bcdb854036e293
orbitaldot <orbitaldotogg@gmail.com>
2024-08-14 20:28:25 +0200

    move motd & install script out of repo

--- scripts/make_minecraft_servers.sh
+++ scripts/make_minecraft_servers.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-# This script takes a Make target as an argument, and then
-# executes it for all Minecraft servers.
-#
-# For example, `bash MAKE_MINECRAFT_SERVERS.sh backup` would
-# execute `make backup` for all of the Minecraft servers.
-
 if [ $# -lt 1 ]; then
     echo "Usage: $0 make_target [command]"
     exit 1
@@ -20,9 +14,9 @@ cd "$(dirname "$0")"
 cd ../minecraft/servers
 
 for server in $MINECRAFT_SERVERS; do
-    cd $server
+    cd "$server"
     if [ "$1" == "command" ]; then
-        COMMAND=$2 make $1
+        COMMAND="$2" make "$1"
     else
         make $1
     fi
